855d2dd4ba9720683d54e21111bb359ae8b63a43
sarahjjust <siriusjjust@gmail.com>
2023-09-11 15:18:56 -0400

    Moved text update into a function.

--- translate.js
+++ translate.js
@@ -1,12 +1,16 @@
 const uri = 'https://localhost:7071/Translation';
+//const uri = 'api/Translation'
 
 function translate() {
     const inputWord = document.querySelector("#toTranslate").value;
     let answer = "";
     fetch(uri)
         .then(response => response.text())
-        .then(data => answer = data)
+        .then(_update)
         .catch(error => console.error('Unable to get items.', error));
+}
+
+function _update(data) {
     const answerSpace = document.querySelector("#answer");
-    answerSpace.innerHTML = answer;
+    answerSpace.innerHTML = data;
 }
