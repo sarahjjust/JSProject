d53ad0809a48f248a0b9870b47f451f6821d4082
sarahjjust <siriusjjust@gmail.com>
2023-09-10 22:08:04 -0400

    Changed translate.js to include api call.

--- translate.js
+++ translate.js
@@ -1,5 +1,12 @@
+const uri = 'https://localhost:7071/Translation';
+
 function translate() {
     const inputWord = document.querySelector("#toTranslate").value;
+    let answer = "";
+    fetch(uri)
+        .then(response => response.text())
+        .then(data => answer = data)
+        .catch(error => console.error('Unable to get items.', error));
     const answerSpace = document.querySelector("#answer");
-    answerSpace.innerHTML = inputWord;
+    answerSpace.innerHTML = answer;
 }
