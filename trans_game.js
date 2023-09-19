2c762ec5a9c4ff202b26c1dbca82dbf34123005b
sarahjjust <siriusjjust@gmail.com>
2023-09-19 16:22:31 -0400

    trans_game完了しました

--- trans_game.js
+++ trans_game.js
@@ -9,12 +9,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const uri = 'https://localhost:7071/Translation';
+const numButtons = 3;
+let answer = -1;
+// Called everytime the page loads. Accesses the API twice, to get three random EN words and the JP translation of one of them.
+// Updates the page with those words.
+// Stores the correct answer in global variable.
 function resetGame() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const response = yield fetch(uri + '/rand/en/3');
+            const response = yield fetch(uri + '/rand/en/' + numButtons);
             const data = yield response.json();
-            const answer = _updateButtons(data);
+            _updateButtons(data);
+            answer = Math.floor(Math.random() * numButtons);
             const response2 = yield fetch(uri + '/en/jp/' + data[answer]);
             const toTrans = yield response2.text();
             _updateJPWord(toTrans);
@@ -24,19 +30,25 @@ function resetGame() {
         }
     });
 }
+// Called when a button is pressed. Displays a positive or negative message depending on if the button was correct. Then reloads the page.
 function checkAnswer(buttonNum) {
-    console.log(buttonNum);
+    if (buttonNum == answer) {
+        window.alert("That's correct!");
+    }
+    else {
+        window.alert("Sorry, that's not correct :(");
+    }
+    location.reload();
 }
+// Updates the text on the buttons.
 function _updateButtons(labels) {
-    const numButtons = labels.length;
     let currButton;
     for (let i = 0; i < numButtons; i++) {
         currButton = document.querySelector('#b' + i);
         currButton.innerHTML = labels[i];
     }
-    const answer = Math.floor(Math.random() * numButtons);
-    return answer;
 }
+// Updates the page to display the word for the player to translate.
 function _updateJPWord(word) {
     const jpWord = document.querySelector('#jpWord');
     jpWord.innerHTML = word;
--- trans_game.ts
+++ trans_game.ts
@@ -1,10 +1,16 @@
 const uri = 'https://localhost:7071/Translation';
+const numButtons = 3;
+let answer = -1;
 
+// Called everytime the page loads. Accesses the API twice, to get three random EN words and the JP translation of one of them.
+// Updates the page with those words.
+// Stores the correct answer in global variable.
 async function resetGame() : Promise<void> {
     try{
-        const response = await fetch(uri + '/rand/en/3');
+        const response = await fetch(uri + '/rand/en/' + numButtons);
         const data = await response.json();
-        const answer = _updateButtons(data);
+        _updateButtons(data);
+        answer = Math.floor(Math.random() * numButtons);
         const response2 = await fetch(uri + '/en/jp/' + data[answer])
         const toTrans = await response2.text();
         _updateJPWord(toTrans);
@@ -13,23 +19,26 @@ async function resetGame() : Promise<void> {
     }
 }
 
+// Called when a button is pressed. Displays a positive or negative message depending on if the button was correct. Then reloads the page.
 function checkAnswer(buttonNum: number) : void {
-    console.log(buttonNum);
+    if (buttonNum == answer) {
+        window.alert("That's correct!");
+    } else {
+        window.alert("Sorry, that's not correct :(")
+    }
+    location.reload();
 }
 
-function _updateButtons(labels : string[]) : number {
-    const numButtons = labels.length;
-
+// Updates the text on the buttons.
+function _updateButtons(labels : string[]) : void {
     let currButton : HTMLButtonElement;
     for (let i = 0; i < numButtons; i++) {
         currButton = document.querySelector('#b' + i)!;
         currButton.innerHTML = labels[i];
     }
-
-    const answer = Math.floor(Math.random() * numButtons);
-    return answer;
 }
 
+// Updates the page to display the word for the player to translate.
 function _updateJPWord(word : string) : void {
     const jpWord = document.querySelector('#jpWord')!;
     jpWord.innerHTML = word;
